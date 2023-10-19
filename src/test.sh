72fdeab1461b1d9ec294ae0a443882c541bd2f77
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-10-19 16:55:02 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 set -euo pipefail
+
+curl -X POST \
+  -H "Authorization: Token token=$BUILDKITE_ANALYTICS_TOKEN" \
+  -F "format=json" \
+  -F "data=@test-results.json" \
+  -F "run_env[CI]=buildkite" \
+  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
+  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
+  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
+  -F "run_env[branch]=$BUILDKITE_BRANCH" \
+  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
+  -F "run_env[message]=$BUILDKITE_MESSAGE" \
+  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
+  https://analytics-api.buildkite.com/v1/uploads
