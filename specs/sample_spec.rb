448a8bb69e198659f0a4509dbaecfbe55c18c1d2
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-10-19 15:21:44 -0600

    Install and set up Buildkite Test Analytics

--- specs/sample_spec.rb
+++ specs/sample_spec.rb
@@ -1,7 +1,14 @@
 require "rspec"
 
+require "buildkite/test_collector"
+
+Buildkite::TestCollector.configure(hook: :rspec)
+
+
 RSpec.describe "A sample test" do
   it "runs aok" do
     expect(42).to eql(42)
   end
-end
+require "buildkite/test_collector"
+
+
