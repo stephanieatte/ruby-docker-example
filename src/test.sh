b9d46b70df0ae3e1318ce0af8197b0eb747f5c4d
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-10-19 17:02:18 -0600

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 set -euo pipefail
 
-curl -X POST \
-  -H "Authorization: Token token=$BUILDKITE_ANALYTICS_TOKEN" \
-  -F "format=json" \
-  -F "data=@test-results.json" \
-  -F "run_env[CI]=buildkite" \
-  -F "run_env[key]=$BUILDKITE_BUILD_ID" \
-  -F "run_env[number]=$BUILDKITE_BUILD_NUMBER" \
-  -F "run_env[job_id]=$BUILDKITE_JOB_ID" \
-  -F "run_env[branch]=$BUILDKITE_BRANCH" \
-  -F "run_env[commit_sha]=$BUILDKITE_COMMIT" \
-  -F "run_env[message]=$BUILDKITE_MESSAGE" \
-  -F "run_env[url]=$BUILDKITE_BUILD_URL" \
-  https://analytics-api.buildkite.com/v1/uploads
+  docker run \
+    --env BUILDKITE_ANALYTICS_TOKEN \
+    --env BUILDKITE_BUILD_ID \
+    --env BUILDKITE_BUILD_NUMBER \
+    --env BUILDKITE_JOB_ID \
+    --env BUILDKITE_BRANCH \
+    --env BUILDKITE_COMMIT \
+    --env BUILDKITE_MESSAGE \
+    --env BUILDKITE_BUILD_URL \
+    bundle exec rspec
+
