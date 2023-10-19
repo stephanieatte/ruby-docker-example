98e948229f2e060f0dc6f88694db2b67782b8b3c
stephanieatte <122297064+stephanieatte@users.noreply.github.com>
2023-10-19 16:53:19 -0600

    add

--- specs/sample_spec.rb
+++ specs/sample_spec.rb
@@ -1,5 +1,10 @@
 require "rspec"
 
+require "buildkite/test_collector"
+
+Buildkite::TestCollector.configure(hook: :rspec)
+
+
 RSpec.describe "A sample test" do
   it "runs aok" do
     expect(42).to eql(42)
